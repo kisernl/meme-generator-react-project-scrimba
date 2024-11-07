defe0a064bf86c1302fbad2ce30b79949d1d9aaa
Noah Kiser <noah.kiser@gmail.com>
2024-11-07 11:55:42 -0600

    add spi fetch to project

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,4 +1,4 @@
-import memesData from "../memesData";
+// import memesData from "../memesData";
 import React from "react";
 
 function Meme() {
@@ -10,38 +10,54 @@ function Meme() {
   // }
 
   /**
-   * Challenge 2: Update our state to save the meme-related
-   * data as an object called `meme`. It should have the
-   * following 3 properties:
-   * topText, bottomText, randomImage.
+   * Challenge 4:
+   * As soon as the Meme component loads the first time,
+   * make an API call to "https://api.imgflip.com/get_memes".
    *
-   * The 2 text states can default to empty strings for now,
-   * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+   * When the data comes in, save just the memes array part
+   * of that data to the `allMemes` state
    *
-   * Next, create a new state variable called `allMemeImages`
-   * which will default to `memesData`, which we imported above
+   * Think about if there are any dependencies that, if they
+   * changed, you'd want to cause to re-run this function.
    *
-   * Lastly, update the `getMemeImage` function and the markup
-   * to reflect our newly reformed state object and array in the
-   * correct way.
+   * Hint: for now, don't try to use an async/await function.
+   * Instead, use `.then()` blocks to resolve the promises
+   * from using `fetch`. We'll learn why after this challenge.
    */
-
   const [meme, setMeme] = React.useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
-  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+  const [allMemes, setAllMemes] = React.useState([]);
+
+  React.useEffect(function () {
+    console.log("Effect ran");
+    fetch(`https://api.imgflip.com/get_memes`)
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
 
   function getMemeImage() {
-    const memeList = allMemeImages.data.memes;
-    const randomMeme = Math.floor(Math.random() * memeList.length);
-    const randomImgUrl = memeList[randomMeme].url;
+    // const memeList = allMemes.data.memes;
+    const randomMeme = Math.floor(Math.random() * allMemes.length);
+    const randomImgUrl = allMemes[randomMeme].url;
     setMeme((prevMeme) => ({
       ...prevMeme,
       randomImage: randomImgUrl,
     }));
+  }
+
+  function handleChange(event) {
+    // console.log(event);
+    const { name, value } = event.target;
+    setAllMemes((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
   }
 
   return (
@@ -53,8 +69,10 @@ function Meme() {
             <input
               className="meme--top"
               type="text"
-              id="top"
-              name="top"
+              id="topText"
+              name="topText"
+              value={allMemes.topText}
+              onChange={handleChange}
             ></input>
           </div>
           <div className="meme--right">
@@ -62,8 +80,10 @@ function Meme() {
             <input
               className="meme--bottom"
               type="text"
-              id="bottom"
-              name="bottom"
+              id="bottomText"
+              name="bottomText"
+              value={allMemes.bottomText}
+              onChange={handleChange}
             ></input>
           </div>
         </div>
@@ -72,13 +92,17 @@ function Meme() {
           <img src="../PhotoIcon.svg" className="meme--btn-img" />
         </button>
       </div>
-      {allMemeImages !== "" && (
-        <img
-          src={meme.randomImage}
-          alt="random meme image"
-          className="meme--img"
-        />
-      )}
+      <div className="meme">
+        {allMemes !== "" && (
+          <img
+            src={meme.randomImage}
+            alt="random meme image"
+            className="meme--img"
+          />
+        )}
+        <h2 className="meme--text top">{allMemes.topText}</h2>
+        <h2 className="meme--text bottom">{allMemes.bottomText}</h2>
+      </div>
     </main>
   );
 }
@@ -105,3 +129,28 @@ export default Meme;
 //   setMemeImage(randomImgUrl);
 //   console.log(memeImage);
 // };
+
+/**
+ * Challenge 2: Update our state to save the meme-related
+ * data as an object called `meme`. It should have the
+ * following 3 properties:
+ * topText, bottomText, randomImage.
+ *
+ * The 2 text states can default to empty strings for now,
+ * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+ *
+ * Next, create a new state variable called `allMemeImages`
+ * which will default to `memesData`, which we imported above
+ *
+ * Lastly, update the `getMemeImage` function and the markup
+ * to reflect our newly reformed state object and array in the
+ * correct way.
+ */
+
+/**
+ * Challenge3:
+ * 1. Set up the text inputs to save to
+ *    the `topText` and `bottomText` state variables.
+ * 2. Replace the hard-coded text on the image with
+ *    the text being saved to state.
+ */
